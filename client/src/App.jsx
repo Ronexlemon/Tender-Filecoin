9d058c68fe4b7dec35cc10c2fe6be51eabe9ef80
dennohkim <kimathidennis38@gmail.com>
2023-02-15 22:16:30 +0300

    Initialize react app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+
+
+function App() {
+  return (
+    <div className="App">
+    <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
