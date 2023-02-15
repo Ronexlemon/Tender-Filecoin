45bf8bec642121cb452bc502c6784550faab9f12
RonexLemon <ronexondimu@gmail.com>
2023-02-15 12:19:48 +0300

    define deployment contract

---
+++ backend/scripts/tenderdeploy.js
@@ -0,0 +1,15 @@
+const {ethers} = require("hardhat");
+
+async function main(){
+    //get the contract
+    //deploy the contract
+    //await deployment
+    //console the address
+}
+//call main
+main().then(()=>
+process.exit(0))
+.catch((error)=>{
+    console.error(error);
+    process.exit(1);
+})
