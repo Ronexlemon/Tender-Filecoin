5cf70d318c23982844708528ef5e4e94c770a159
anthonykimani <kimaniantoni@gmail.com>
2023-02-17 13:13:22 +0300

    Adds Navbar component

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+
+const HeroSection = () => {
+    return ( 
+        <div className="bg-[url('./assets/svg/hero-section.svg')] h-screen w-full bg-no-repeat bg-cover">
+            <Navbar />
+        </div>
+     );
+}
+ 
+export default HeroSection;
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection';
 
 const LandingPage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <HeroSection />
     </div>
   );
 }
