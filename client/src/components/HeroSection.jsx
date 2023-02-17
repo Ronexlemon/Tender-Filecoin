d1d69d13c1787002c764be1957d5581228db9ea2
anthonykimani <kimaniantoni@gmail.com>
2023-02-17 18:42:57 +0300

    Adds Feature Section

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,3 +1,4 @@
+import FeatureSection from "./FeatureSection";
 import HeroText from "./HeroText";
 import Navbar from "./Navbar";
 
@@ -6,6 +7,7 @@ const HeroSection = () => {
         <div className="bg-[url('./assets/svg/hero-section.svg')] w-full bg-no-repeat bg-cover">
             <Navbar />
             <HeroText />
+            <FeatureSection />
         </div>
      );
 }
