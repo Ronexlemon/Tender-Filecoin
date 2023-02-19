eafb24bbab586ce8d6419e427c647b332619fda2
RonexLemon <ronexondimu@gmail.com>
2023-02-19 16:16:44 +0300

    change ABI and contract Address

--- client/src/pages/tenderpost/Tenders.jsx
+++ client/src/pages/tenderpost/Tenders.jsx
@@ -15,7 +15,7 @@ const Tenders = () => {
   let btnapprove = useRef(null);
   //let Tenders =[];
   const [Tenders, setTenders] = useState([]);
-  const TenderOwnerAddress = "0x8fF171857abe05f4642e90Ec243A9553f0853678";
+  const TenderOwnerAddress = "0xE50A2E68f31e899D6e794314823cD2ac126BD764";
   const [tenderslength, setLength] = useState(0);
   const web3ModalRef = useRef();
   const [walletconnect, setWalletConnect] = useState(false);
@@ -55,9 +55,9 @@ const Tenders = () => {
     const web3Provider = new providers.Web3Provider(provider);
     //check if user is connected to Mumbai network
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 80001) {
-      window.alert("Change network to Mumbai");
-      throw new Error("Change network To Mumbai");
+    if (chainId !== 3141) {
+      window.alert("Change network to HyperSpace fileCoin");
+      throw new Error("Change network To HyperSpace fileCoin ");
     }
     // alert("network is Mumbai")
     //if need signer for transactions
