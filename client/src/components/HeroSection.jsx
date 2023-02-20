686b516193b9344a175bbf2168f2239a0768771e
anthonykimani <kimaniantoni@gmail.com>
2023-02-20 21:26:57 +0300

    updates Herosection

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -4,12 +4,13 @@ import Footer from "./Footer";
 import HeroText from "./HeroText";
 import MapSection from "./MapSection";
 import Navbar from "./Navbar";
+import NavbarLanding from "./NavbarLanding";
 import SubscribeSection from "./SubscribeSection";
 
 const HeroSection = () => {
     return ( 
         <div className="bg-[url('./assets/svg/hero-section.svg')] w-full bg-no-repeat bg-cover">
-            <Navbar />
+            <NavbarLanding />
             <HeroText />
             <FeatureSection />
             <BusinessSection />
