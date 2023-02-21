dac3e537f999daf6353ab73a4a4e666652dcd53b
RonexLemon <ronexondimu@gmail.com>
2023-02-21 12:43:22 +0300

    redeploy contract

--- backend/scripts/tenderdeploy.js
+++ backend/scripts/tenderdeploy.js
@@ -1,6 +1,7 @@
 const {ethers} = require("hardhat");
 //1.TenderFileConContractAddress 0x05198c2783d3497361ca936a70E5643287dfD0B8
 //2. TenderFileConContractAddress 0xE50A2E68f31e899D6e794314823cD2ac126BD764
+//3. TenderFileConContractAddress 0x1F949e4688F0933B699899a04ad4f9E76112b560
 
 async function main(){
     //get the contract
