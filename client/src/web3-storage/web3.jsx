4618800ad32387006f166d6646cff9262ce0462a
RonexLemon <ronexondimu@gmail.com>
2023-02-24 12:52:40 +0300

    upload file

--- client/src/web3-storage/web3.jsx
+++ client/src/web3-storage/web3.jsx
@@ -1,6 +1,8 @@
 import "./web3.css";
 import { useState } from "react";
 import { create as ipfsHttpClient } from "ipfs-http-client";
+import { Web3Storage, getFilesFromPath } from 'web3.storage'
+
 
 
 
@@ -11,15 +13,40 @@ const projectId = "2M5aWABDDME6TZYRmrMEMftAbdq"
 const projectSecretKey = "91a42de39accf97776173ae66975e3ca"
 const authorization = "Basic " + btoa(projectId + ":" + projectSecretKey);
 
-function Web3() {
-  const [uploadedImages, setUploadedImages] = useState([]);
-  const ipfs = ipfsHttpClient({
-    url: "https://filecoin.infura.io",
-    headers: {
-      authorization,
-    },
-  });
-  const onSubmitHandler = async (event) => {
+ function Web3() {
+   const [uploadedImages, setUploadedImages] = useState([]);
+   const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJFRjRiMTdhYzY1MjgzNEYxQTBkMTQxNTUwOTRlYTdiYTMzRWEyOWIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzcyMzA1NTE0NTMsIm5hbWUiOiJ0ZW5kZXJzcGFjZSJ9.CwbHkp79KAwCjQTpRmlRJWSWKa10VBSJLLv4eMrmVJs";
+  // const ipfs = ipfsHttpClient({
+  //   url: "https://filecoin.infura.io",
+  //   headers: {
+  //     authorization,
+  //   },
+  // });
+  // const onSubmitHandler = async (event) => {
+  //   event.preventDefault();
+  //   const form = event.target;
+  //   const files = form[0].files;
+
+  //   if (!files || files.length === 0) {
+  //     return alert("No files selected");
+  //   }
+
+  //   const file = files[0];
+  //   // upload files
+  //   const result = await ipfs.add(file);
+
+  //   setUploadedImages([
+  //     ...uploadedImages,
+  //     {
+  //       cid: result.cid,
+  //       path: result.path,
+  //     },
+  //   ]);
+
+  //   form.reset();
+  // };
+  
+    const onSubmitHandler = async (event) => {
     event.preventDefault();
     const form = event.target;
     const files = form[0].files;
@@ -28,25 +55,20 @@ function Web3() {
       return alert("No files selected");
     }
 
-    const file = files[0];
-    // upload files
-    const result = await ipfs.add(file);
-
-    setUploadedImages([
-      ...uploadedImages,
-      {
-        cid: result.cid,
-        path: result.path,
-      },
-    ]);
-
-    form.reset();
-  };
+    
+    const storage = new Web3Storage({ token })
+     
+  
+    console.log(`Uploading ${files.length} files`)
+    const cid = await storage.put(files)
+    console.log('Content added with CID:', "https://"+cid+".ipfs.w3s.link")
+  }
+  
 
   return (
     <div className="app">
       <div className="app__container">
-        {ipfs ? (
+        {  (
           <div className="container">
             <h1>IPFS uploader</h1>
             <form onSubmit={onSubmitHandler}>
@@ -59,7 +81,7 @@ function Web3() {
               </button>
             </form>
           </div>
-        ) : null}
+        ) }
         <div className="data">
           {uploadedImages.map((image, index) => (
             <>
@@ -71,9 +93,9 @@ function Web3() {
                 key={image.cid.toString() + index}
               />
               <h4>Link to IPFS:</h4>
-              <a href={"https://filecoin.infura.io" + image.path}>
+              {/* <a href={"https://filecoin.infura.io" + image.path}>
                 <h3>{"https://filecoin.infura.io" + image.path}</h3>
-              </a>
+              </a> */}
             </>
           ))}
         </div>
